83d68383c0f12e0cf2b590aacc40bde09fd5a262
Luke Rollans <luke@rollans.sh>
2023-07-27 13:27:29 +0800

    add script phases boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import fs from "fs"
 import assert from "assert"
-import { emojify } from "node-emoji"
+
+import {
+  ECS,
+  waitUntilTasksRunning,
+  waitUntilTasksStopped,
+} from "@aws-sdk/client-ecs"
 
 import * as core from "@actions/core"
 
@@ -27,9 +32,50 @@ async function run(): Promise<void> {
       "`AWS_SECRET_ACCESS_KEY` is not set in the environment. Has a previous action setup AWS credentials?"
     )
 
-    // 1. register the task's definition
+    // ensure AWS_REGION was picked up from the environment
+    assert(
+      process.env.AWS_REGION,
+      "`AWS_REGION` is not set in the environment. Has a previous action setup AWS credentials?"
+    )
 
-    // 2. run the task, and wait for it to complete
+    // run the nominated task and collect the results
+    const ecs = new ECS({ region: process.env.AWS_REGION })
+    const { tasks, failures } = await ecs.runTask({
+      taskDefinition: taskDefPath,
+    })
+
+    // log failures if they exist
+    if (failures && failures.length > 0) {
+      core.info(`${failures.length} tasks did not start successfully`)
+    }
+
+    // take action on tasks successfully requested
+    if (tasks) {
+      // grab the task ARNs
+      const taskArns = tasks.map((task) => task.taskArn!)
+
+      // wait for tasks to begin running
+      await core.group("Waiting for RUNNING state...", async () => {
+        await waitUntilTasksRunning(
+          { client: ecs, maxWaitTime: 60 },
+          { tasks: taskArns }
+        )
+      })
+
+      // update logs
+      core.info("All tasks have entered RUNNING state")
+
+      // wait for tasks to complete and report the result
+      await core.group("Waiting for STOPPED state...", async () => {
+        const waiter = await waitUntilTasksStopped(
+          { client: ecs, maxWaitTime: 3600 },
+          { tasks: taskArns }
+        )
+        if (waiter.state !== "SUCCESS") {
+          core.setFailed(waiter.reason.toString())
+        }
+      })
+    }
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message)
